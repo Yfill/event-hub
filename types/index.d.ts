ca30258ebb80c35e81c1b18fb2bea48f33a57ab8
Yfill <vps998@gmail.com>
2020-11-18 01:26:05 +0800

    build: build 1.0.2

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,13 +1,15 @@
 declare type Handler = (...arg: any[]) => void;
 export default class EventHub {
-    static on: Function;
-    static off: Function;
-    static emit: Function;
+    static on: (type: string, handler: Handler) => void;
+    static once: (type: string, handler: Handler) => void;
+    static off: (type: string, handler?: Handler | undefined) => void;
+    static emit: (type: string, ...arg: any[]) => void;
     static create(): EventHub;
     private id;
     private status;
     constructor();
     on(type: string, handler: Handler): EventHub;
+    once(type: string, handler: Handler): EventHub;
     off(type: string, handler?: Handler): EventHub;
     emit(type: string, ...arg: any[]): EventHub;
     destroy(): void;
